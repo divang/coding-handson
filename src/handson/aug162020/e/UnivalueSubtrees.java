b0e7d89822ff11217ba29ecf1e80ed6e9689c397
sdivang <sdivang@192.168.1.4>
2020-08-17 16:23:40 +0530

    Decode String

--- src/handson/aug162020/e/UnivalueSubtrees.java
+++ src/handson/aug162020/e/UnivalueSubtrees.java
@@ -18,23 +18,20 @@ public class UnivalueSubtrees {
 		}
 	}
 
+	int count = 0;
+	boolean is_valid_part(TreeNode node, int val) {
+
+		if (node == null)
+			return true;
+		if (!is_valid_part(node.left, node.val)
+				| !is_valid_part(node.right, node.val))
+			return false;
+		count++;
+		return node.val == val;
+	}
 	public int countUnivalSubtrees(TreeNode root) {
-		if (root == null)
-			return 0;
-
-		if (root.left == null && root.right == null)
-			return 1;
-
-		int count = 0;
-
-		if (root.left != null && root.left.val == root.val)
-			count += 1;
-
-		if (root.right != null && root.right.val == root.val)
-			count += 1;
-
-		return count + countUnivalSubtrees(root.left)
-				+ countUnivalSubtrees(root.right);
+		is_valid_part(root, 0);
+		return count;
 	}
 
 }
